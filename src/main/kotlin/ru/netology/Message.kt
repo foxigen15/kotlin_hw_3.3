f795746a4fab3f59e402c341bac9b98fb37dcaa1
Alexey Gulyaev <algulaev@mail.ru>
2022-04-12 07:54:51 +0300

    fun findMessageById modified

--- src/main/kotlin/ru/netology/Message.kt
+++ src/main/kotlin/ru/netology/Message.kt
@@ -6,7 +6,7 @@ data class Message(
     val ownerId: Int,
     val recipientId: Int,
     val text: String,
-    val itIsRead: Boolean = false,
+    val itIsRead: Boolean = true,
     val itIsDelete: Boolean = false
 ) {
 }
